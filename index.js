82f04b79e8f53cacab0e1caa58edf1315681641d
beka tsalani <beqacalani@gmail.com>
2017-10-07 14:27:07 +0400

    added ssl support

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ var LocalStrategy = require('passport-local').Strategy;
 var cmsRoutes = require('./app/routes/cms');
 var blogRouter = require('./app/routes/blog');
 var expressValidator = require('express-validator');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+var options = {
+  key: '',
+  cert: ''
+};
+
+fs.readFile('test/fixtures/keys/agent2-key.pem', function(data){
+  if(data){
+    options.key = data;
+  }
+});
+fs.readFile('test/fixtures/keys/agent2-cert.cert',function(data){
+  if(data){
+    options.cert = data;
+  }
+});
+
 
 mongoose.connect('mongodb://localhost/cms');
 
@@ -49,4 +69,6 @@ app.use(function(req,res,next){
 app.use('/cms',cmsRoutes);
 app.use('/',blogRouter);
 
-app.listen(8080);
+http.createServer(app).listen(8081);
+
+https.createServer(options, app).listen(3000);
